c0561b807bcbe6d8672c15898e8b0e278fa8b129
ruanifsilva <ruanifsilva@gmail.com>
2021-11-30 09:34:24 -0300

    corrigindo erros na view resultado_enquete

--- app/urls.py
+++ app/urls.py
@@ -2,15 +2,17 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+app_name = 'app'
+
 router = routers.DefaultRouter()
 router.register(r'enquetes', views.EnqueteViewSet)
 router.register(r'respostas', views.RespostaViewSet)
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('<int:pk>', views.Detalhes.as_view(), name='detalhes'),
-    # path('resultado_enquete/<int:pk>/', views.ResultadoEnquete.as_view(), name='resultado'),
-    path('resultado_enquete/<int:detalhe_id>/', views.resultado_enquete, name='resultado_enquete'),
+    path('<int:pk>/', views.Detalhes.as_view(), name='detalhes'),
+    path('<int:pk>/resultado/', views.ResultadoEnquete.as_view(), name='resultado'),
+    path('<int:detalhe_id>/resultado_enquete/', views.resultado_enquete, name='resultado_enquete'),
     path(r'api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Enquete, Resposta
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from django.views import generic
+
 
 # importações para REST
-
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
@@ -12,9 +11,7 @@ from .serializers import EnqueteSerializer, RespostaSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
 
-
-
-class Index(ListView):
+class Index(generic.ListView):
     template_name = 'app/index.html'
     context_object_name = 'lista_enquetes'
 
@@ -22,29 +19,34 @@ class Index(ListView):
         return Enquete.objects.order_by('-titulo')
 
 
-class Detalhes(DetailView):
+class Detalhes(generic.DetailView):
     model = Enquete
     template_name = 'app/detalhe_enquete.html'
     context_object_name = 'enquete'
 
 
-# class ResultadoEnquete(Index):
-#     template_name = 'app/resultado_enquete.html'
-#     context_object_name = 'enquete'
+class ResultadoEnquete(generic.DetailView):
+    model = Enquete
+    template_name = 'app/resultado_enquete.html'
+    # context_object_name = 'enquete'
 
 
 def resultado_enquete(request, detalhe_id):
     enquete = get_object_or_404(Enquete, id=detalhe_id)
 
     if request.method != 'POST':
-        return render(request, 'app/delhate_enquete.html')
+        return render(request, 'app/delhate_enquete.html', {'enquete': enquete})
 
     resposta = request.POST.get('resposta')
+
+    if not resposta:
+        return redirect('app:index')
+
     resposta_selecionada = enquete.respostas.get(id=resposta)
     resposta_selecionada.votos += 1
     resposta_selecionada.save()
 
-    return render(request, 'app/resultado_enquete.html', {'enquete': enquete})
+    return redirect(reverse('app:resultado', kwargs={'pk': enquete.id}))
 
 
 class EnqueteViewSet(viewsets.ModelViewSet):
